a71772ad4257fc843851f9ae1e5d071f942c964b
mike <mikefc@coolbutuseless.com>
2024-10-19 08:33:05 +1000

    active queue

--- src/poisson.c
+++ src/poisson.c
@@ -59,10 +59,10 @@ void init_points(points_t *p) {
   }
 }
 
-void add_point(points_t *p, double x, double y) {
+int add_point(points_t *p, double x, double y) {
   
   if (p->idx >= p->capacity) {
-    p->capacity = 2 * p->capacity;
+    p->capacity *= 2;
     p->x = realloc(p->x, p->capacity * sizeof(double));
     p->y = realloc(p->y, p->capacity * sizeof(double));
     if (p->x == NULL || p->y == NULL) {
@@ -73,6 +73,9 @@ void add_point(points_t *p, double x, double y) {
   p->x[p->idx] = x;
   p->y[p->idx] = y;
   p->idx++;
+  
+  
+  return p->idx - 1;
 }
 
 void free_points(points_t *p) {
@@ -81,6 +84,73 @@ void free_points(points_t *p) {
   free(p->x);
   free(p->y);
 }
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Active list
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+typedef struct {
+  int *list;
+  int capacity;
+  int idx;
+} active_t;
+
+void init_active(active_t *active) {
+  active->capacity = 1024;
+  active->idx = 0;
+  active->list = calloc(active->capacity, sizeof(int));
+  if (active->list == NULL) {
+    error("Couldn't allocate 'active'");
+  }
+}
+
+void add_active(active_t *active, int point_idx) {
+  
+  if (active->idx >= active->capacity) {
+    active->capacity *= 2;
+    active->list = realloc(active->list, active->capacity * sizeof(int));
+    if (active->list == NULL) {
+      error("Coudln't reallocate active");
+    }
+  }
+  
+  active->list[active->idx] = point_idx;
+  active->idx++;
+}
+
+void free_active(active_t *active) {
+  if (active == NULL) return;
+  free(active->list);
+}
+
+
+void remove_active(active_t *active, int active_idx) {
+  
+  if (active_idx >= active->idx) {
+    error("Out of bounds");
+  }
+  
+  // Move the last item into this position to be removed
+  active->idx--;
+  active->list[active_idx] = active->list[active->idx];
+}
+
+
+int random_active(active_t *active, int *active_idx) {
+  
+  if (active->idx == 0) {
+    error("An attempt was made to sample from an empty active list");  
+  }
+  
+  GetRNGstate();
+  double rand = unif_rand();
+  PutRNGstate();
+  
+  *active_idx = (int)floor(rand * active->idx);
+  return active->list[*active_idx];
+}
+
+
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -112,12 +182,50 @@ SEXP poisson2d_(SEXP w_, SEXP h_, SEXP r_, SEXP k_) {
     grid[i] = -1;
   }
   
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Initialise points
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   points_t p = { 0 };
   init_points(&p);
   
-  for (int i = 0; i < 200; i++) {
+  for (int i = 0; i < 10; i++) {
     add_point(&p, (double)i, (double)(i + 1));
   }
+  
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // Initialise active list
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  active_t active = {0};
+  init_active(&active);
+  add_active(&active, 0);
+  add_active(&active, 1);
+  add_active(&active, 2);
+  add_active(&active, 3);
+  add_active(&active, 4);
+  
+  for (int i = 0; i < 10; i++) {
+    int active_idx = 0;
+    int point_idx = random_active(&active, &active_idx);
+    Rprintf("Sample: [%i] = %i\n", active_idx, point_idx);
+  }  
+  
+  Rprintf("\n");
+  remove_active(&active, 2);
+  
+  for (int i = 0; i < 10; i++) {
+    int active_idx = 0;
+    int point_idx = random_active(&active, &active_idx);
+    Rprintf("Sample: [%i] = %i\n", active_idx, point_idx);
+  }  
+  
+  Rprintf("\n");
+  remove_active(&active, 0);
+  
+  for (int i = 0; i < 10; i++) {
+    int active_idx = 0;
+    int point_idx = random_active(&active, &active_idx);
+    Rprintf("Sample: [%i] = %i\n", active_idx, point_idx);
+  }  
   
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -134,11 +242,14 @@ SEXP poisson2d_(SEXP w_, SEXP h_, SEXP r_, SEXP k_) {
   SET_VECTOR_ELT(res_, 0, x_);
   SET_VECTOR_ELT(res_, 1, y_);
   
+  memcpy(REAL(x_), p.x, p.idx * sizeof(double));
+  memcpy(REAL(y_), p.y, p.idx * sizeof(double));
   
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // Tidy and return
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   free_points(&p);
+  free_active(&active);
   UNPROTECT(nprotect);
   return res_;
 }
