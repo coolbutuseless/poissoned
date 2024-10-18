8369e9cb659263eeab2c1b7c30d1b4028c65c0e8
mike <mikefc@coolbutuseless.com>
2024-10-19 06:17:39 +1000

    C framework

---
+++ src/init.c
@@ -0,0 +1,26 @@
+
+// #define R_NO_REMAP
+#include <R.h>
+#include <Rinternals.h>
+
+SEXP poisson2d_(SEXP w_, SEXP h_, SEXP r_, SEXP k_);
+
+static const R_CallMethodDef CEntries[] = {
+  {"poisson2d_", (DL_FUNC) &poisson2d_, 4},
+  {NULL , NULL, 0}
+};
+
+
+void R_init_poissoned(DllInfo *info) {
+  R_registerRoutines(
+    info,      // DllInfo
+    NULL,      // .C
+    CEntries,  // .Call
+    NULL,      // Fortran
+    NULL       // External
+  );
+  R_useDynamicSymbols(info, FALSE);
+}
+
+
+
---
+++ src/poisson.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include <R.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+
+
+SEXP poisson2d_(SEXP w_, SEXP h_, SEXP r_, SEXP k_) {
+  return R_NilValue;
+}
+
